725130e6bc19123ccfd5114d47416634fbaff0a9
michalmazgal <140933865+michalmazgal@users.noreply.github.com>
2023-08-15 17:24:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -85,4 +85,6 @@ echo "\"y\" on the question to save and \"enter\""
 echo "on the name"
 
 echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
-~/.ccminer/start.sh
+
+cd ~/ccminer
+./start.sh\
